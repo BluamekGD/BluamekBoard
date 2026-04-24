a8376843d1ab5ec72f6362a4c6de4f40c5d30954
BluamekGD <bartosz.nicely@gmail.com>
2026-04-24 20:05:58 +0200

    got a basic square

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 use macroquad::prelude::*;
 
-#[macroquad::main("Hello World")]
+#[macroquad::main("BetterBoard :)")]
 
 async fn main() {
     loop{
-        if is_key_pressed(KeyCode::W) {
-        println!("w pressed")
+        draw_rectangle(screen_width() / 2.0 - 90.0, 10.0, 60.0, 60.0, DARKGRAY);
+        if is_key_down(KeyCode::W) {
+        draw_rectangle(screen_width() / 2.0 - 90.0, 10.0, 60.0, 60.0, LIGHTGRAY);
         }
         next_frame().await
     }
