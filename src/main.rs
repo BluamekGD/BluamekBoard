464fbaa433131e31f7a5690b0a7d2f232a0a8511
BluamekGD <bartosz.nicely@gmail.com>
2026-04-25 10:07:42 +0200

    window configuration

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,19 @@
 use macroquad::prelude::*;
 
-#[macroquad::main("BetterBoard :)")]
+// Window configuration
+fn window_conf() -> Conf {
+    Conf {
+        window_title: "BetterBoard :)".to_owned(),
+        window_width: 400,
+        window_height: 200,
+        ..Default::default()
+    }
+}
 
+// Create window
+#[macroquad::main(window_conf)]
+
+// Draw W square
 async fn main() {
     loop{
         draw_rectangle(screen_width() / 2.0 - 90.0, 10.0, 60.0, 60.0, DARKGRAY);
